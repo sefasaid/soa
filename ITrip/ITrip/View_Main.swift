bb0aa9de20e5ca4d4dfef8c64bade395aa49e4d7
ahmetsaday <ahmetsaday34@gmail.com>
2017-03-25 00:03:10 +0300

    Main ekran yapısını oluşturdum

---
+++ ITrip/ITrip/View_Main.swift
@@ -0,0 +1,19 @@
+//
+//  View_Main.swift
+//  ITrip
+//
+//  Created by osx on 24.03.2017.
+//  Copyright © 2017 Özgün. All rights reserved.
+//
+
+import UIKit
+
+class View_Main: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
