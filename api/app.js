6c14ed3443d38b82e4fd06ff7c1a07fe8c1885d0
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-03-24 23:07:38 +0300

    Şehirler için basit bir api yapıldı

--- api/app.js
+++ api/app.js
@@ -1,3 +1,26 @@
 /**
  * Created by sefasaid on 24/03/2017.
  */
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    morgan = require('morgan'),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+mongoose.connect("mongodb://localhost/soa");
+
+var Resource = app.resource = restful.model('city', mongoose.Schema({
+    name: String
+})).methods(['get', 'post', 'put', 'delete']);
+
+Resource.register(app, '/api/city');
+
+app.listen(3000);
