ef183d17a2728dc7dc80037f9682b85487597c11
Özgün <ergenozgun@gmail.com>
2017-03-26 13:57:58 +0300

    Cell hücreleri bağlandı

--- ITrip/ITrip/Cell_FindParticipant.swift
+++ ITrip/ITrip/Cell_FindParticipant.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 
 class Cell_FindParticipant: UITableViewCell {
@@ -15,7 +16,7 @@ class Cell_FindParticipant: UITableViewCell {
     var lbl_UserName        = UILabel()
     var lbl_Message         = UILabel()
     var lbl_UserTags        = UILabel()
-    
+    var tags = OOETag?
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -34,6 +35,38 @@ class Cell_FindParticipant: UITableViewCell {
     func setCreateUI(){
     
         self.addSubview(self.img_UserProfile)
+        self.img_UserProfile.frame = CGRect(x: 15, y: 30, width: 60, height: 60)
+        self.img_UserProfile.backgroundColor = UIColor.grayColor()
+        self.img_UserProfile.layer.masksToBounds = true
+        self.img_UserProfile.layer.cornerRadius = 30
+        
+        
+        self.addSubview(lbl_UserName)
+        lbl_UserName.snp_makeConstraints { (make) in
+            make.left.equalTo(img_UserProfile.snp_right).offset(15)
+            make.top.equalTo(self).offset(10)
+            make.right.equalTo(self).offset(-10)
+            make.height.equalTo(25)
+        }
+        lbl_UserName.text = "O.Özgün Ergen"
+        lbl_UserName.font = UIFont.appBoldFont(14)
+        lbl_UserName.textColor = UIColor.flatBlackColor()
+        
+        
+        self.addSubview(lbl_Message)
+        lbl_Message.snp_makeConstraints { (make) in
+            make.left.equalTo(img_UserProfile.snp_right).offset(15)
+            make.top.equalTo(lbl_UserName.snp_bottom).offset(5)
+            make.right.equalTo(self).offset(-10)
+            make.bottom.equalTo(self).offset(-25)
+        }
+        lbl_Message.text = "lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet"
+        lbl_Message.font = UIFont.appFont(11)
+        lbl_Message.textColor = UIColor.flatGrayColor()
+        lbl_Message.numberOfLines = 5
+        
+        
+        
         
         
     }
